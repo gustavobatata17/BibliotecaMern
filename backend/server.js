e1cb4c0c16bd8b58e747ab760839498b4a5b0e34
Gustavo José Costa Gregenski <133015470+gustavobatata17@users.noreply.github.com>
2024-10-30 13:54:14 +0000

    projeto concluido

--- backend/server.js
+++ backend/server.js
@@ -1,20 +1,38 @@
 import "dotenv/config";
-import express from "express";
-import mongoose from "mongoose";
 
-export async function connectToDatabase() {
-  try {
-    await mongoose.connect(process.env.MONGO_URI);
-    console.log("Connected to the database");
-  } catch (error) {
-    console.error("Error connecting to the database: ", error);
-    process.exit(1);
-  }
-}
+import path from "path";
+import express from "express";
+
+import { connectToDatabase } from "./config/db.js";
+import bookRouter from "./routes/book.routes.js";
+
+const port = process.env.PORT || 3000;
+const __dirname = path.resolve();
 
 const app = express();
 
-app.listen(3000, () => {
-  console.log("Server is running on port 3000");
+app.use(express.json());
+
+app.get("/health", (req, res) => {
+  try {
+    res.status(200).send("Server is running");
+  } catch (error) {
+    console.error("Error checking server health:", error);
+    res.status(500).send("Server is not running");
+  }
+});
+
+app.use("/api/v1/books", bookRouter);
+
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static(path.join(__dirname, "/frontend/dist")));
+
+  app.get("*", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "frontend", "dist", "index.html"));
+  });
+}
+
+app.listen(port, () => {
+  console.log(`Server is running on port ${port}`);
   connectToDatabase();
 });
